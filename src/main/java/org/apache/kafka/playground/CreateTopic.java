ca9ebbeab0147f29a1346fe38ad97bf58b137598
Paolo Patierno <ppatierno@live.com>
2017-12-24 15:56:57 +0100

    Added running local Kafka cluster to the CreateTopic example

--- src/main/java/org/apache/kafka/playground/CreateTopic.java
+++ src/main/java/org/apache/kafka/playground/CreateTopic.java
@@ -1,5 +1,24 @@
+/**
+ * Copyright 2017 Paolo Patierno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.ù
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.kafka.playground;
 
+import io.debezium.kafka.KafkaCluster;
+import io.debezium.util.Testing;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
@@ -7,6 +26,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.errors.TimeoutException;
 import org.apache.kafka.common.errors.TopicExistsException;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -14,6 +34,15 @@ import java.util.concurrent.ExecutionException;
 public class CreateTopic {
 
     public static void main(String[] args) throws Exception {
+
+        File dataDir = Testing.Files.createTestingDirectory("cluster");
+
+        KafkaCluster kafkaCluster = new KafkaCluster()
+                .usingDirectory(dataDir)
+                .withPorts(2181, 9092)
+                .deleteDataPriorToStartup(true)
+                .addBrokers(1)
+                .startup();
 
         Properties props = new Properties();
         props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -34,6 +63,9 @@ public class CreateTopic {
             e.printStackTrace();
         } finally {
             adminClient.close();
+
+            kafkaCluster.shutdown();
+            dataDir.delete();
         }
     }
 }
