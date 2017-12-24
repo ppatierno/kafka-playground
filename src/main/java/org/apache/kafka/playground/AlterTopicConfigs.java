8edeec46dc44bc5a110b505d1f513f3c6dc840de
Paolo Patierno <ppatierno@live.com>
2017-12-24 15:48:00 +0100

    Added running local Kafka cluster to the AlterTopicConfigs example

--- src/main/java/org/apache/kafka/playground/AlterTopicConfigs.java
+++ src/main/java/org/apache/kafka/playground/AlterTopicConfigs.java
@@ -1,5 +1,24 @@
+/**
+ * Copyright 2017 Paolo Patierno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.ù
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.kafka.playground;
 
+import io.debezium.kafka.KafkaCluster;
+import io.debezium.util.Testing;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.AlterConfigsResult;
@@ -9,6 +28,7 @@ import org.apache.kafka.clients.admin.DescribeConfigsResult;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.TopicConfig;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +41,17 @@ import java.util.stream.Collectors;
 public class AlterTopicConfigs {
 
     public static void main(String[] args) throws Exception {
+
+        File dataDir = Testing.Files.createTestingDirectory("cluster");
+
+        KafkaCluster kafkaCluster = new KafkaCluster()
+                .usingDirectory(dataDir)
+                .withPorts(2181, 9092)
+                .deleteDataPriorToStartup(true)
+                .addBrokers(1)
+                .startup();
+
+        kafkaCluster.createTopic("test", 1, 1);
 
         Properties props = new Properties();
         props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -67,5 +98,8 @@ public class AlterTopicConfigs {
         System.out.println(retentionEntry.value());
 
         adminClient.close();
+
+        kafkaCluster.shutdown();
+        dataDir.delete();
     }
 }
