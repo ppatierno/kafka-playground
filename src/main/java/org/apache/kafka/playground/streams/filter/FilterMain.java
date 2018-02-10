a7d9f76577beb4ec705abbeb90e4d27e4b6ed15b
Paolo Patierno <ppatierno@live.com>
2018-02-10 13:03:24 +0100

    Added simple filtering example

---
+++ src/main/java/org/apache/kafka/playground/streams/filter/FilterMain.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 Paolo Patierno
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.ù
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.kafka.playground.streams.filter;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class FilterMain {
+
+    public void main(String[] args) {
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("source-topic");
+
+        source.filter((k, v) -> Integer.valueOf(k) > 5)
+                .to("sink-topic");
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "filter-stream-id");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        StreamsConfig config = new StreamsConfig(props);
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                streams.close();
+            }
+        });
+
+        streams.start();
+    }
+}
