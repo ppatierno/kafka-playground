84c6251e62860b898f402d494c2831ad7529d9d3
Paolo Patierno <ppatierno@live.com>
2019-11-03 11:39:17 +0100

    Added Printed usage example

---
+++ src/main/java/org/apache/kafka/playground/streams/PrintedExample.java
@@ -0,0 +1,45 @@
+package org.apache.kafka.playground.streams;
+
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+
+/**
+ * PrintedExample
+ */
+public class PrintedExample {
+
+    public static void main(String[] args) {
+        
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> uppers = builder
+                                            .stream("input", Consumed.with(Serdes.String(), Serdes.String()))
+                                            .mapValues(v -> v.toUpperCase());
+
+        uppers.to("upper", Produced.with(Serdes.String(), Serdes.String()));
+
+        Printed sysout = Printed.toSysOut().withLabel("stdout");
+        uppers.print(sysout);
+
+        Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, PrintedExample.class.getSimpleName().replace("$", ""));
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, PrintedExample.class.getSimpleName().replace("$", ""));
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        KafkaStreams ks = new KafkaStreams(topology, props);
+        ks.start();
+    }
+}
